673ab07998247c88b3e1e3e4dd906a6d454fa0fb
Zowie <me@zowie.cx>
2021-05-16 16:24:59 +0200

    initial commit

---
+++ src/Utility.ts
@@ -0,0 +1,12 @@
+export function flagSet(input: number, flag: number): boolean {
+  return (input & flag) == flag
+}
+export type Result<T> = T | Error
+
+export function isError<T>(result: Result<T>): result is Error {
+  return result instanceof Error
+}
+
+export function isSuccess<T>(result: Result<T>): result is T {
+  return !isError(result)
+}
